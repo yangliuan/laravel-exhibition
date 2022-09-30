e2c094421a79494da1d77e7c4c9910d383ec8da5
yangliuan <yangliuancn@foxmail.com>
2022-09-30 20:31:26 +0800

    md

--- app/Events/AuthDeviceEvent.php
+++ app/Events/AuthDeviceEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class AuthDeviceEvent
+class AuthDeviceEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
